3c85c199fa3b308119a65448003a9176b3e3fb84
Rimah Houssameldine <rimahhd3@gamil.com>
2024-05-06 16:52:47 +0300

    add getters and setters

--- HR-Management-System/src/main/java/com/example/HRManagementSystem/HrManagementSystemApplication.java
+++ HR-Management-System/src/main/java/com/example/HRManagementSystem/HrManagementSystemApplication.java
@@ -1,13 +1,40 @@
 package com.example.HRManagementSystem;
 
+import com.example.HRManagementSystem.Repositories.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
-public class HrManagementSystemApplication {
+@EnableJpaRepositories(basePackages = {"com.example.HRManagementSystem.Repositories"})
+@EntityScan(basePackages = {"com.example.HRManagementSystem.Entities"})
+@ComponentScan(basePackages = {"com.example.HRManagementSystem.Mappers", "com.example.HRManagementSystem.Controllers", "com.example.HRManagementSystem.Services"})
+public class HrManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrManagementSystemApplication.class, args);
+
 	}
 
+
+	private EmployeeRepository employeeRepository;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+	public EmployeeRepository getEmployeeRepository() {
+		return employeeRepository;
+	}
+
+	public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
 }
+
